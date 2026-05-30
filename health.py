c17fbb58e605c32c23f73ba9e56d1c7cbf0fa171
unsido10 <unsido10@gmail.com>
2026-05-30 10:46:00 +0300

    Update health.py

--- health.py
+++ health.py
@@ -8,6 +8,6 @@ def health():
     return "OK", 200
 
 if __name__ == '__main__':
-    # Render передает свой порт в переменной PORT
+    # Обязательно слушаем 0.0.0.0 и порт, который дает Render
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
